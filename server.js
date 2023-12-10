ed85e5371d5ec223eb6d893edc416096a615eb1b
edenlewis <edenlewis80@gmail.com>
2023-12-10 14:54:40 -0700

    started adding front end routes and get routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const  {readFromFile}= require('./helpers/fsUtils')
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,15 +13,20 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to serve up static assets from the public folder
 app.use(express.static('public'));
 
-
-
+// api routes
+app.get('/api/notes', (req, res) =>{
+readFromFile('./db/db.json').then((data)=> res.json(JSON.parse(data)))
+});
 
 // This view route is a GET route for the notes page
 app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
+// This view route is a GET route for the homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
